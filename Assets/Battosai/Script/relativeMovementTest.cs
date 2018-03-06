5259cedfbf6256e3cbf749c115330248f7d6901d
TheCell <simon_hischier@hotmail.com>
2018-03-06 10:23:19 +0100

    added terrain movement test

---
+++ Assets/Battosai/Script/relativeMovementTest.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class relativeMovementTest : MonoBehaviour {
+
+	private Transform objectTransform;
+	public float deltaMetersPerSecond = 2.0f;
+	private bool positiveDir = false;
+
+	// Use this for initialization
+	void Start()
+	{
+		objectTransform = GetComponent<Transform>();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		Vector3 newPos = objectTransform.position;
+
+		if (positiveDir)
+		{
+			newPos = newPos + objectTransform.up * deltaMetersPerSecond * Time.deltaTime;
+		}
+		else
+		{
+			newPos = newPos - objectTransform.up * deltaMetersPerSecond * Time.deltaTime;
+		}
+
+		if (Vector3.Magnitude(newPos) > 100.0f)
+		{
+			positiveDir = !positiveDir;
+		}
+
+		objectTransform.position = newPos;
+	}
+}
