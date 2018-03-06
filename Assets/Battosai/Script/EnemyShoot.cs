4d0fe82b9c19605a057c57676f52615a0d4406df
micromic5 <mike.amacker@gmx.ch>
2018-03-06 14:44:43 +0100

    Enemy will shoot in Intrervalls

--- Assets/Battosai/Script/EnemyShoot.cs
+++ Assets/Battosai/Script/EnemyShoot.cs
@@ -5,15 +5,37 @@ using UnityEngine;
 public class EnemyShoot : MonoBehaviour
 {
     public GameObject prefab;
-    public float bulletSpeed = 0.8f;
-
+    public float bulletSpeed = 0.6f;
+    public int timeBetweenShoots = 2;
+    public int minTimeBetweenBursts = 5;
+    public int maxTimeBetweenBursts = 10;
+    public int minAmountOfShootsInOneBurst = 1;
+    public int maxAmountOfShootsInOneBurst = 6;
+    private bool canShoot = true;
+    private int shootCounter = 0;
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        if (Input.GetButtonDown("Fire1"))
+        
+        if (canShoot)
+        {            
+            canShoot = false;
+            shootCounter = Random.Range(minAmountOfShootsInOneBurst, maxAmountOfShootsInOneBurst);
+            Invoke("shoot", Random.Range(minTimeBetweenBursts, maxTimeBetweenBursts));
+        }
+    }
+
+    private void shoot()
+    {
+        GameObject attack = Instantiate(prefab, transform.position, Quaternion.identity);
+        attack.GetComponent<Rigidbody>().AddForce(bulletSpeed * (GameObject.Find("Camera (eye)").transform.position - attack.transform.position), ForceMode.Impulse);
+        shootCounter--;
+        if (shootCounter >= 0)
         {
-            GameObject attack = Instantiate(prefab, transform.position, Quaternion.identity);
-            attack.GetComponent<Rigidbody>().AddForce(bulletSpeed * (GameObject.Find("Camera (eye)").transform.position - attack.transform.position), ForceMode.Impulse);
+            Invoke("shoot", timeBetweenShoots);
+        }else
+        {
+            canShoot = true;
         }
     }
 }
