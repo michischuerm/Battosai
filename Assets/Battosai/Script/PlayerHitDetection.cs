e13eef93fb2d872faae9f43788f0b4c276e9110a
micromic5 <mike.amacker@gmx.ch>
2018-03-07 11:10:08 +0100

    Added Particles and Area Attack Logik to the enemy

--- Assets/Battosai/Script/PlayerHitDetection.cs
+++ Assets/Battosai/Script/PlayerHitDetection.cs
@@ -5,43 +5,66 @@ using UnityEngine;
 public class PlayerHitDetection : MonoBehaviour {
     public GameObject prefabSafeSphere;
     private GameObject sphere;
-    private float currentSphereSize = 0.25f;
+    private float currentSphereSize = .1f;
+    private float startSphereSize = .1f;
+    public float sphereGrowSpeed = 0.02f;
+    private float maxSphereSize = 1f;
+    public float timeTillSphereIsDestroyedAfterMaxSize = 1f;
+    private float timeTillSphereIsDestroyedCounter = 0f;
     public int hp = 3;
     public bool isHit = false;
 
+    void OnParticleTrigger()
+    {
+        Debug.Log("trigger with particle");
+    }
     void OnTriggerEnter(Collider col)
     {
+        Debug.Log(col.gameObject.name);
         if(col.gameObject.tag == "makeDamage")
         {
-            hp--;
-            if (hp <= 0)
-            {
-                Debug.Log("you lose");
-            }else
-            {
-                isHit = true;
-                sphere = Instantiate(prefabSafeSphere, transform.position, Quaternion.identity);
-                sphere.transform.parent = this.transform;
-                sphere.transform.localScale = new Vector3(currentSphereSize, currentSphereSize, currentSphereSize);
-                foreach(GameObject threat in GameObject.FindGameObjectsWithTag("makeDamage")){
-                    Destroy(threat);
-                }
-            }
+            gotHit();
         }
     }
 
     private void Update()
     {
-        if(sphere != null && currentSphereSize <=2)
+        if(sphere != null && currentSphereSize <= maxSphereSize)
         {
-            currentSphereSize += Time.deltaTime*0.3f;
+            currentSphereSize += Time.deltaTime * sphereGrowSpeed;
             sphere.transform.localScale = new Vector3(currentSphereSize, currentSphereSize, currentSphereSize);
+        }
+        else if(sphere != null && timeTillSphereIsDestroyedCounter < timeTillSphereIsDestroyedAfterMaxSize)
+        {
+            timeTillSphereIsDestroyedCounter += Time.deltaTime;
         }
         else if(sphere != null)
         {
+            timeTillSphereIsDestroyedCounter = 0f;
             isHit = false;
             Destroy(sphere);
-            currentSphereSize = 0.25f;
+            currentSphereSize = startSphereSize;
+        }
+    }
+
+    public void gotHit()
+    {
+        hp--;
+        if (hp <= 0)
+        {
+            //Teleport player back where he found the weapons and let him pick them up again to get teleported to the fight
+            Debug.Log("you lose");
+        }
+        else
+        {
+            isHit = true;
+            sphere = Instantiate(prefabSafeSphere, transform.position, new Quaternion(1, 0, 0, 1));
+            sphere.transform.parent = this.transform;
+            sphere.transform.localScale = new Vector3(currentSphereSize, currentSphereSize, currentSphereSize);
+            foreach (GameObject threat in GameObject.FindGameObjectsWithTag("makeDamage"))
+            {
+                Destroy(threat);
+            }
         }
     }
 }
