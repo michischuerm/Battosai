989dcb3e3f2d8615388cc4bc050767672846526f
micromic5 <mike.amacker@gmx.ch>
2018-03-07 15:50:40 +0100

    added shoot dmg to the player bullets so he can kill the dragon

---
+++ Assets/Battosai/Script/EnemyHPHandler.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHPHandler : MonoBehaviour {
+    public int hp = 100;
+    public int stageTwo = 50;
+    public void takeDamage(int damage)
+    {
+        hp-=damage;
+        if(hp == stageTwo)
+        {
+            GetComponent<staticEnemy>().enabled = true;
+        }
+    }
+
+}
---
+++ Assets/Battosai/Script/playerBullet.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerBullet : MonoBehaviour {
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "enemy")
+        {
+
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
