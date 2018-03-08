4e13cc1a1cf3739e47c0e6c3dbf4dd768baf83dd
micromic5 <mike.amacker@gmx.ch>
2018-03-08 15:18:24 +0100

    modified cameraRig, shield should block enemy attacks now

---
+++ Assets/Battosai/Script/shieldBlock.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shieldBlock : MonoBehaviour {
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.name == "BossOneBreathAttack")
+        {
+            Destroy(other);
+        }
+    }
+}
