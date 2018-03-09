0220c3a1f0028cb401b6572bc3ccdf050d054e8d
TheCell <simon_hischier@hotmail.com>
2018-03-09 10:07:46 +0100

    debug sphere for each controller

--- Assets/Battosai/Script/stationaryControll.cs
+++ Assets/Battosai/Script/stationaryControll.cs
@@ -7,8 +7,8 @@ public class stationaryControll : MonoBehaviour
 	public Transform stationaryFixedHinge;
 	private GameObject controllerInteractionBox;
 	private List<SteamVR_TrackedObject> trackedObjs;
-	private SteamVR_TrackedObject controller1;
-	private SteamVR_TrackedObject controller2;
+	private List<SteamVR_TrackedObject> controllers;
+	private List<GameObject> debugSpheres;
 	private int controllersInBox = 0;
 
 	// Use this for initialization
@@ -16,6 +16,8 @@ public class stationaryControll : MonoBehaviour
 	{
 		controllerInteractionBox = gameObject;
 		trackedObjs = new List<SteamVR_TrackedObject>();
+		controllers = new List<SteamVR_TrackedObject>();
+		debugSpheres = new List<GameObject>();
 	}
 
 	// Update is called once per frame
@@ -25,11 +27,14 @@ public class stationaryControll : MonoBehaviour
 
 		if (controllersInBox > 0)
 		{
+			updateTrackedControllers(trackedObjs);
+			drawDebugSpheresAtControllers();
 			Debug.Log("controllersInBox: " + controllersInBox);
 		}
 
 		if (controllersInBox >= 2)
 		{
+			Debug.Log("at least two controller active: " + controllers);
 
 		}
 	}
@@ -88,10 +93,64 @@ public class stationaryControll : MonoBehaviour
 	{
 		if (SteamVR_Controller.Input((int)trackedObj.index) == null)
 		{
-			Debug.Log("no index");
+			//Debug.Log("no index");
+			return false;
 		}
 
-		Debug.Log("tracked obj as controller: " + trackedObj);
+		//Debug.Log("tracked obj as controller: " + trackedObj);
 		return true;
+	}
+
+	private void updateTrackedControllers(List<SteamVR_TrackedObject> trackedObjToCheck)
+	{
+		List<SteamVR_TrackedObject> controllerList = new List<SteamVR_TrackedObject>();
+
+		trackedObjToCheck.ForEach(delegate (SteamVR_TrackedObject obj)
+		{
+			if (isController(obj))
+			{
+				controllerList.Add(obj);
+			}
+		});
+
+		// overwrite current controllers with the new list
+		controllers = controllerList;
+	}
+
+	private void drawDebugSpheresAtControllers()
+	{
+		// create more spheres when there are not enough prepared to display
+		while (debugSpheres.Count < controllersInBox)
+		{
+			GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+			sphere.transform.position = new Vector3(0, 0F, 0);
+			sphere.transform.localScale = new Vector3(0.015f, 0.015f, 0.015f);
+			debugSpheres.Add(sphere);
+		}
+
+		// set as many spheres to controller positions as there are controllers
+		IEnumerator sphereEnumerator = debugSpheres.GetEnumerator();
+		IEnumerator controllersEnumerator = controllers.GetEnumerator();
+		if (controllersEnumerator.MoveNext())
+		{
+			if (sphereEnumerator.MoveNext())
+			{
+				GameObject sphere = (GameObject)sphereEnumerator.Current;
+				sphere.SetActive(true);
+				SteamVR_TrackedObject controller = (SteamVR_TrackedObject) controllersEnumerator.Current;
+				sphere.transform.position = controller.transform.position;
+			}
+			else
+			{
+				Debug.Log("less spheres then controllers");
+			}
+		}
+
+		// disable spheres that are not used
+		while (sphereEnumerator.MoveNext())
+		{
+			GameObject sphereToDisable = (GameObject)sphereEnumerator.Current;
+			sphereToDisable.SetActive(false);
+		}
 	}
 }
