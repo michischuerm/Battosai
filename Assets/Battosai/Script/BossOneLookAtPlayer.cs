d21e702b2bc3350e94248c3f2c6b896cc34ef2b6
micromic5 <mike.amacker@gmx.ch>
2018-03-09 14:04:59 +0100

    Test different headturning solution

--- Assets/Battosai/Script/BossOneLookAtPlayer.cs
+++ Assets/Battosai/Script/BossOneLookAtPlayer.cs
@@ -14,6 +14,8 @@ public class BossOneLookAtPlayer : MonoBehaviour {
     private float correctionTimer = 0;
     private float maxCorrectionTimer = 1;
     private Transform LookatPositionMonster;
+    public bool canLookAtPlayer = false;
+
     void Start()
     {
         monsterNeck = GameObject.Find("Neck").transform;
@@ -23,15 +25,34 @@ public class BossOneLookAtPlayer : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if(startLookDirectionTimer)
+       /* if(startLookDirectionTimer)
         {
             lookDirectionTimer += Time.deltaTime;
-        }
+        }*/
     }
 
     private void LateUpdate()
     {
-        if (lookDirectionTimer <= maxLookDirectionTimer)
+        if (canLookAtPlayer)
+        {
+            float x = monsterNeck.localRotation.eulerAngles.x;
+            float y = monsterNeck.localRotation.eulerAngles.y;
+            float z = monsterNeck.localRotation.eulerAngles.z;
+            x -= x > 180 ? 360 : 0;
+            z -= z > 180 ? 360 : 0;
+            if (Mathf.Abs(y) < 140 && Mathf.Abs(y) > 40
+                && Mathf.Abs(x) < 80
+                && Mathf.Abs(z) < 80)
+            {
+              //  startLookDirectionTimer = false;
+                //Rotate the head to lookto the player
+                monsterNeck.rotation = Quaternion.Lerp(monsterNeck.rotation, Quaternion.LookRotation(-target.position + monsterNeck.position), Time.deltaTime * 2);
+            }
+        }else
+        {
+            monsterNeck.rotation = Quaternion.Lerp(monsterNeck.rotation, Quaternion.LookRotation(-LookatPositionMonster.position + monsterNeck.position), Time.deltaTime * 2);
+        }
+        /*if (lookDirectionTimer <= maxLookDirectionTimer)
         {
             float x = monsterNeck.localRotation.eulerAngles.x;
             float y = monsterNeck.localRotation.eulerAngles.y;
@@ -61,6 +82,6 @@ public class BossOneLookAtPlayer : MonoBehaviour {
                 lookDirectionTimer = 0f;
                 correctionTimer = 0;
             }
-        }
+        }*/
     }
 }
