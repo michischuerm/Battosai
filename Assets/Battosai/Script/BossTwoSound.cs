223714f10bacffb769c29d6a77abc117b059e5a2
micromic5 <mike.amacker@gmx.ch>
2018-03-14 10:01:28 +0100

    boss2 audio speed changes with the distance to the player

---
+++ Assets/Battosai/Script/BossTwoSound.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossTwoSound : MonoBehaviour {
+    private AudioSource audio;
+    public float maxDistanceToThePlayer = 22;
+    private GameObject target;
+
+	// Use this for initialization
+	void Start () {
+        audio = GetComponent<AudioSource>();
+        target = GameObject.Find("Camera (eye)");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        audio.pitch = Map(maxDistanceToThePlayer - Vector3.Distance(target.transform.position, transform.position),0,maxDistanceToThePlayer,0.8f,1.7f);
+    }
+    
+    private float Map(float value, float from1, float to1, float from2, float to2)
+    {
+        return (value - from1) / (to1 - from1) * (to2 - from2) + from2;
+    }
+}
