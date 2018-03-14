a8235064e2b97f15d9fedefaae30548592593b81
micromic5 <mike.amacker@gmx.ch>
2018-03-14 14:18:53 +0100

    changed update to fixedupdate

--- Assets/Battosai/Script/BossOneWinder.cs
+++ Assets/Battosai/Script/BossOneWinder.cs
@@ -15,7 +15,7 @@ public class BossOneWinder : MonoBehaviour {
     }
 
     // Update is called once per frame
-    void Update () {
+    void FixedUpdate () {
         MoveEnemy();
     }
     private void MoveEnemy()
