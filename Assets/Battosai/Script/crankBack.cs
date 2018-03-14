f557f937efa70c7254df432bbbbe6c7ec7ddf543
TheCell <simon_hischier@hotmail.com>
2018-03-14 13:39:41 +0100

    update crank calculation

--- Assets/Battosai/Script/crankBack.cs
+++ Assets/Battosai/Script/crankBack.cs
@@ -4,16 +4,22 @@ using UnityEngine;
 
 public class crankBack : MonoBehaviour
 {
-	public float distanceCranked = 0.0f;
-	public float crankToDistanceFactor = 2.0f;
+	public float distanceToStartCranked = 0.0f;
+	public float crankToDistanceFactor = 1.0f;
+	public float fixedUpdateDistanceDelta = 0.0f;
 	private HingeJoint hingeJoint;
+	private float radius;
 
 	// Use this for initialization
 	void Start ()
 	{
 		hingeJoint = GetComponent<HingeJoint>();
 		
-		if (hingeJoint == null)
+		if (hingeJoint != null)
+		{
+			radius = Vector3.Magnitude(hingeJoint.anchor);
+		}
+		else
 		{
 			Debug.Log("No Hinge Joint on this Object");
 		}
@@ -22,9 +28,16 @@ public class crankBack : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
 	{
-		Debug.Log("velocity: " + hingeJoint.velocity);
-		Debug.Log("angle: " + hingeJoint.angle);
-		distanceCranked += crankToDistanceFactor * hingeJoint.velocity;
-		Debug.Log("distanceCranked: " + distanceCranked);
+		
+	}
+
+	private void FixedUpdate()
+	{
+		// distance travelled this update. calulating the arc and set the sign for the direction
+		this.fixedUpdateDistanceDelta = hingeJoint.angle / 360 * Mathf.PI * 2 * radius * Mathf.Sign(hingeJoint.velocity);
+		// calculated distance travelled with factor. Relative to the start.
+		this.distanceToStartCranked += crankToDistanceFactor * fixedUpdateDistanceDelta;
+		//Debug.Log("distanceCranked: " + distanceToStartCranked);
+		Debug.Log("fixedUpdateDistanceDelta: " + fixedUpdateDistanceDelta);
 	}
 }
