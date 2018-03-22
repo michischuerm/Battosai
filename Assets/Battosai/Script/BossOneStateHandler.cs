fad14de136f337f008778a1091a2c847b3daad29
micromic5 <mike.amacker@gmx.ch>
2018-03-22 14:27:01 +0100

    state change particle effect is destroyed after 4 seconds

--- Assets/Battosai/Script/BossOneStateHandler.cs
+++ Assets/Battosai/Script/BossOneStateHandler.cs
@@ -35,14 +35,14 @@ public class BossOneStateHandler : MonoBehaviour {
         }
         else if(newState == 2 && state < 2)
         {
+            GetComponent<EnemyShoot>().enabled = false;
             Transform spawnPoint = GameObject.Find("BossOne/Rig/WingPart/Neck/Head").transform;
 
-            GameObject attackEffect = Instantiate(prefab, spawnPoint.position, new Quaternion(0, 0, 0, 0));
+            attackEffect = Instantiate(prefab, spawnPoint.position, new Quaternion(0, 0, 0, 0));
             attackEffect.transform.position += new Vector3(0, -0.5f, 0);
             attackEffect.transform.SetParent(spawnPoint, true);
             attackEffect.transform.LookAt(GameObject.Find("LookStraight").transform.position);
-            attackEffect.SetActive(true);
-            Invoke("destroyEffect", 3);
+            Invoke("startAttack",1);
 
             GetComponent<EnemyMovementAI>().movementSpeed  /= 10;
             GetComponent<EnemyMovementAI>().rotationStrength /= 10;
@@ -93,10 +93,17 @@ public class BossOneStateHandler : MonoBehaviour {
         state = newState;
     }
 
+    void startAttack()
+    {
+        attackEffect.SetActive(true);
+        Invoke("destroyEffect", 3);
+    }
     void destroyEffect()
     {
         GetComponent<EnemyMovementAI>().movementSpeed = GetComponent<EnemyMovementAI>().originalMovementSpeed;
         GetComponent<EnemyMovementAI>().rotationStrength = GetComponent<EnemyMovementAI>().originalRotationStrength;
+        attackEffect.SetActive(false);
         Destroy(attackEffect);
+        GetComponent<EnemyShoot>().enabled = true;
     }
 }
