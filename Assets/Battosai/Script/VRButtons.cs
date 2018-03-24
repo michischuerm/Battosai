33b70d68d5941ca39dfdd7dcba32bf4964122334
TheCell <simon_hischier@hotmail.com>
2018-03-24 18:05:12 +0100

    added startup Menu

---
+++ Assets/Battosai/Script/VRButtons.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class VRButtons : MonoBehaviour
+{
+	public Material hitMaterial;
+	private Material normalMaterial;
+	public Button thisButton;
+
+	// Use this for initialization
+	void Start ()
+	{
+		normalMaterial = GetComponent<Renderer>().material;
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		GetComponent<Renderer>().material = hitMaterial;
+
+		switch(thisButton)
+		{
+			case Button.Start:
+				SceneManager.LoadScene("level_1", LoadSceneMode.Single);
+				break;
+			case Button.Exit:
+				Application.Quit();
+				break;
+			case Button.Tutorial:
+				SceneManager.LoadScene("level_tutorial", LoadSceneMode.Single);
+				break;
+			case Button.Level1:
+				SceneManager.LoadScene("level_1", LoadSceneMode.Single);
+				break;
+			case Button.Level2:
+				SceneManager.LoadScene("level_2", LoadSceneMode.Single);
+				break;
+			case Button.shootingRange:
+				SceneManager.LoadScene("shootingRange", LoadSceneMode.Single);
+				break;
+			default:
+				break;
+		}
+	}
+
+	private void OnTriggerExit(Collider other)
+	{
+		GetComponent<Renderer>().material = normalMaterial;
+	}
+
+	public enum Button
+	{
+		Start,
+		Exit,
+		Tutorial,
+		Level1,
+		Level2,
+		shootingRange
+	};
+}
