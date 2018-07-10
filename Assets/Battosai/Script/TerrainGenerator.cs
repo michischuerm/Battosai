a1e1f640e7c9599a0be17cf9637318f65a34db8e
Simon <simon_hischier@hotmail.com>
2018-07-10 19:44:25 +0200

    terrain now visible. but triangle edge numbers still wrong

--- Assets/Battosai/Script/TerrainGenerator.cs
+++ Assets/Battosai/Script/TerrainGenerator.cs
@@ -13,14 +13,18 @@ public class TerrainGenerator : MonoBehaviour
     void Start ()
     {
         bool displayDebugPoints = true;
-        print("tileCountX " + tileCountX + " tileCountY " + tileCountZ);
+
+        if (displayDebugPoints)
+        {
+            print("tileCountX " + tileCountX + " tileCountY " + tileCountZ);
+        }
         MeshFilter meshFilter = null;
         Mesh mesh = new Mesh();
         meshFilter = GetComponent<MeshFilter>();
         meshFilter.mesh = mesh;
 
         // generate the vertices
-        int vertexAmount = (tileCountX + tileCountZ + 2) * (tileCountX + tileCountZ + 2);
+        int vertexAmount = (tileCountX + 1) * (tileCountZ + 1);
         Vector3[] vertices = new Vector3[vertexAmount];
         Vector2[] uv = new Vector2[vertexAmount];
         int xVertexNumber = 0;
@@ -61,30 +65,49 @@ public class TerrainGenerator : MonoBehaviour
         // setup the triangles from the vertices
         // every tile is built by 2 triangles
         int triangleAmount = tileCountX * tileCountZ * 2;
+        print("triangleAmount " + triangleAmount);
         int vertexPointsX = tileCountX + 1;
         int vertexPointsY = tileCountZ + 1;
         int[] trianglePoints = new int[triangleAmount * 3];
         int counterForTopTwoPoints = 0;
         int counterForBottomTwoPoints = 0;
+        int trianglePointIndex = 0;
         for (int i = 0; i < triangleAmount; i++)
         {
+            if (displayDebugPoints)
+            {
+                print("trianglePointIndex " + trianglePointIndex);
+            }
+            
             if (i % 2 == 0)
             {
                 // top 2 points are the number half the triangle count and +1
-                trianglePoints[i] = (i / 2);
-                trianglePoints[i + 1] = (i / 2) + 1;
-                trianglePoints[i + 2] = vertexPointsX + counterForTopTwoPoints;
-                print("triangleTop " + (i / 2) + " " + ((i / 2) + 1) + " " + (vertexPointsX + counterForTopTwoPoints));
+                trianglePoints[trianglePointIndex] = (i / 2);
+                trianglePoints[trianglePointIndex + 1] = vertexPointsX + counterForTopTwoPoints;
+                trianglePoints[trianglePointIndex + 2] = (i / 2) + 1;
+                if (displayDebugPoints)
+                {
+                    print("triangleTop " + (i / 2)
+                        + " " + (vertexPointsX + counterForTopTwoPoints)
+                        + " " + ((i / 2) + 1));
+                }
                 counterForTopTwoPoints++;
             }
             else
             {
-                trianglePoints[i] = counterForBottomTwoPoints + 1;
-                trianglePoints[i + 1] = vertexPointsX + counterForBottomTwoPoints + 1;
-                trianglePoints[i + 2] = vertexPointsX + counterForBottomTwoPoints;
-                print("triangleBottom " + (counterForBottomTwoPoints + 1) + " " + (vertexPointsX + counterForBottomTwoPoints + 1) + " " + (vertexPointsX + counterForBottomTwoPoints));
+                trianglePoints[trianglePointIndex] = counterForBottomTwoPoints + 1;
+                trianglePoints[trianglePointIndex + 1] = vertexPointsX + counterForBottomTwoPoints;
+                trianglePoints[trianglePointIndex + 2] = vertexPointsX + counterForBottomTwoPoints + 1;
+                if (displayDebugPoints)
+                {
+                    print("triangleBottom " + (counterForBottomTwoPoints + 1)
+                        + " " + (vertexPointsX + counterForBottomTwoPoints)
+                        + " " + (vertexPointsX + counterForBottomTwoPoints + 1));
+                }
                 counterForBottomTwoPoints++;
             }
+            
+            trianglePointIndex = trianglePointIndex + 3;
         }
 
         mesh.triangles = trianglePoints;
