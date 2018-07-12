1f91e458f9054362faa0802d82b543094edae605
Simon <simon_hischier@hotmail.com>
2018-07-12 19:09:13 +0200

    added noise to terrain, outsourced triangles into function

--- Assets/Battosai/Script/TerrainGenerator.cs
+++ Assets/Battosai/Script/TerrainGenerator.cs
@@ -7,12 +7,24 @@ public class TerrainGenerator : MonoBehaviour
     public int tileCountX = 30;
     public int tileCountZ = 30;
     public float width = 10.0f;
-    public float height = 10.0f;
+    public float length = 10.0f;
+    public float height = 5.0f;
+    private System.Random worldSeedGenerator = new System.Random();
+    public int worldSeed;
 
     // Use this for initialization
     void Start ()
     {
-        bool displayDebug = true;
+        bool displayDebug = false;
+        if (worldSeed == 0)
+        {
+            worldSeed = Random.Range(1, int.MaxValue);
+        }
+        if (displayDebug)
+        {
+            print("worldSeed " + worldSeed);
+        }
+        worldSeedGenerator = new System.Random(worldSeed);
 
         if (displayDebug)
         {
@@ -23,7 +35,7 @@ public class TerrainGenerator : MonoBehaviour
         meshFilter = GetComponent<MeshFilter>();
         meshFilter.mesh = mesh;
 
-        // generate the vertices
+        // generate the vertices and uv maps
         int vertexAmount = (tileCountX + 1) * (tileCountZ + 1);
         Vector3[] vertices = new Vector3[vertexAmount];
         Vector2[] uv = new Vector2[vertexAmount];
@@ -33,8 +45,14 @@ public class TerrainGenerator : MonoBehaviour
         for (int i = 0; i < vertexAmount; i++)
         {
             float xPosition = xVertexNumber * (width / (tileCountX + 1));
-            float zPosition = zVertexNumber * (height / (tileCountZ + 1));
-            vertices[i] = new Vector3(xPosition, 0, zPosition);
+            float zPosition = zVertexNumber * (length / (tileCountZ + 1));
+            Random rnd = new Random();
+            
+            float yPosition = Mathf.PerlinNoise((
+                (1.0f * worldSeedGenerator.Next(1, int.MaxValue)) / int.MaxValue), 
+                ((1.0f * worldSeedGenerator.Next(1, int.MaxValue)) / int.MaxValue)) * height;
+            print(yPosition);
+            vertices[i] = new Vector3(xPosition, yPosition, zPosition);
             uv[i] = new Vector2(xPosition, zPosition);
             if (displayDebug)
             {
@@ -63,14 +81,32 @@ public class TerrainGenerator : MonoBehaviour
         mesh.uv = uv;
 
         // setup the triangles from the vertices
+        mesh.triangles = trianglesFromTiles(tileCountX, tileCountZ, displayDebug);
+
+        Vector3[] normals = new Vector3[vertexAmount];
+        for (int i = 0; i < vertexAmount; i++)
+        {
+            normals[i] = -Vector3.forward;
+        }
+
+        mesh.normals = normals;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    int[] trianglesFromTiles(int tileCountX, int tileCountZ, bool showDebugLogs = false)
+    {
         // every tile is built by 2 triangles
         int triangleAmount = tileCountX * tileCountZ * 2;
-        if (displayDebug)
+        if (showDebugLogs)
         {
             print("triangleAmount " + triangleAmount);
         }
         int vertexPointsX = tileCountX + 1;
-        int vertexPointsY = tileCountZ + 1;
         int[] trianglePoints = new int[triangleAmount * 3];
         int trianglePointIndex = 0;
         int xCounter = 0;
@@ -78,17 +114,17 @@ public class TerrainGenerator : MonoBehaviour
 
         for (int i = 0; i < triangleAmount; i++)
         {
-            if (displayDebug)
+            if (showDebugLogs)
             {
                 print("trianglePointIndex " + trianglePointIndex);
             }
-            
+
             if (i % 2 == 0)
             {
                 trianglePoints[trianglePointIndex] = xCounter + yCounter * vertexPointsX; // x0y0
                 trianglePoints[trianglePointIndex + 1] = xCounter + (yCounter + 1) * vertexPointsX; // x0y1
                 trianglePoints[trianglePointIndex + 2] = (xCounter + 1) + yCounter * vertexPointsX; // x1y0
-                if (displayDebug)
+                if (showDebugLogs)
                 {
                     print("triangleTop " + (xCounter + yCounter * vertexPointsX)
                         + " " + (xCounter + (yCounter + 1) * vertexPointsX)
@@ -100,7 +136,7 @@ public class TerrainGenerator : MonoBehaviour
                 trianglePoints[trianglePointIndex] = (xCounter + 1) + yCounter * vertexPointsX; // x1y0
                 trianglePoints[trianglePointIndex + 1] = xCounter + (yCounter + 1) * vertexPointsX; // x0y1
                 trianglePoints[trianglePointIndex + 2] = (xCounter + 1) + (yCounter + 1) * vertexPointsX; // x1y1
-                if (displayDebug)
+                if (showDebugLogs)
                 {
                     print("triangleBottom " + ((xCounter + 1) + yCounter * vertexPointsX)
                         + " " + (xCounter + (yCounter + 1) * vertexPointsX)
@@ -118,20 +154,6 @@ public class TerrainGenerator : MonoBehaviour
             trianglePointIndex = trianglePointIndex + 3;
         }
 
-        mesh.triangles = trianglePoints;
-
-        Vector3[] normals = new Vector3[vertexAmount];
-        for (int i = 0; i < vertexAmount; i++)
-        {
-            normals[i] = -Vector3.forward;
-        }
-
-        mesh.normals = normals;
-	}
-	
-	// Update is called once per frame
-	void Update ()
-    {
-		
-	}
+        return trianglePoints;
+    }
 }
